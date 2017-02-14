b0f36595c2ea9112aebe14209845b33c7e544c51
JimmyHoldo <jimmy_holdo@hotmail.com>
2017-02-14 22:11:04 +0100

    trace fix

--- Lab2/Example.hs
+++ Lab2/Example.hs
@@ -1,16 +1,33 @@
-module Example where
+module Main where
 
 import Data.Time
 import Replay
+
+main = running emptyTrace example
 
 example :: Replay String String Int
 example = do
   t0 <- io getCurrentTime
   io (putStrLn "Hello!")
   age <- ask "What is your age?"
-  io (putStrLn ("You are " ++ age))
+  io (putStrLn ("You are " ++ (show age)))
   name <- ask "What is your name?"
   io (putStrLn (name ++ " is " ++ age ++ " years old"))
   t1 <- io getCurrentTime
   io (putStrLn ("Total time: " ++ show (diffUTCTime t1 t0)))
   return (read age)
+
+
+running :: Trace String -> Replay String String a -> IO a
+running tr prog = play tr
+ where
+  play t = do
+    r <- run prog t    -- this is the same prog every time!
+    case r of
+      Left (q,t2) -> do
+        --print t2
+        putStr ("Question: " ++ q ++ " ")
+        r <- getLine
+        play (addAnswer t2 r)
+      Right x -> do print t
+                    return x
--- Lab2/Replay.hs
+++ Lab2/Replay.hs
@@ -1,23 +1,32 @@
 {-# LANGUAGE GADTs #-}
-module Replay where
-    
+module Replay (
+    Replay(..)
+    , Trace (..)
+    , Item (..)
+    , addAnswer
+    , emptyTrace
+    , io
+    , ask
+    , run
+    )where
+
 import Control.Applicative (Applicative(..))
-import Control.Monad       (liftM, ap)   
+import Control.Monad       (liftM, ap)
 import Control.Monad.Trans
 import Data.Time
 
-    
+
 -- Types
 data Replay q r a where
     Io     :: (Show a, Read a) =>  IO a -> Replay q r a
-    Ask    :: (Show q, Read q) =>  q -> Replay q r r
+    Ask    :: q -> Replay q r r
     Return :: a -> Replay q r a
     Bind   :: Replay q r a -> (a -> Replay q r b) -> Replay q r b
-    
+
 
 type Trace r = [Item r]
 
-data Item r = Answer r| Result String 
+data Item r = Answer r| Result String
   deriving (Show,Read)
 
 
@@ -27,12 +36,12 @@ data Item r = Answer r| Result String
 
 -- Operations
 instance Monad (Replay q r) where
-    return = Return     
+    return = Return
     (>>=) = Bind
 
 instance Applicative (Replay q r) where
     pure = return
-    (<*>) = ap 
+    (<*>) = ap
 
 instance Functor (Replay q r) where
     fmap = liftM
@@ -49,34 +58,33 @@ run x tr =do e <- run' x tr
 
 run' :: Replay q r a -> Trace r -> IO (Either (q, Trace r, Trace r) (a, Trace r, Trace r))
 run'(Return x)  tr = return (Right (x, tr, []))
-run' (Io x)  tr = 
+run' (Io x)  tr =
     case tr of
         (Result r):trs -> return $ (Right ((read r), trs, [Result r]))
-        trs -> do 
+        trs -> do
             r <- x
             return $ (Right ((r), trs, [Result (show r)]))
-run' (Ask x) tr = 
+run' (Ask x) tr =
     case tr of
         (Answer r):trs -> return $ (Right ((r), trs, [Answer r]))
-        trs -> return $ (Left (x, trs, trs))
-run' (Bind ma f) tr = do 
-    e1 <- run' ma tr 
+        trs -> return $ (Left (x, trs, []))
+run' (Bind ma f) tr = do
+    e1 <- run' ma tr
     case e1 of
-        (Left (x, tr, tr2)) -> do return $ (Left (x, tr, tr))
-        (Right (x, tr, tr2)) -> do 
+        (Left (x, tr, tr2)) -> return $ (Left (x, tr, tr2))
+        (Right (x, tr, tr2)) -> do
             e <- run' (f x) tr
             case e of
-              (Left (y, tr', tr2')) -> return $ (Left ((y), tr2', tr2 ++ tr2'))
-              (Right (y, tr', tr2')) -> return $ (Right ((y), tr2', tr2 ++ tr2'))
-
+              (Left (y, tr', tr2')) -> return $ (Left (y, tr2++tr', tr2 ++ tr2'))
+              (Right (y, tr', tr2')) -> return $ (Right (y, tr', tr2 ++ tr2'))
 
 io  :: (Show a, Read a) => IO a -> Replay q r a
 io = Io
 
-                
-ask :: (Show q, Read q) =>  q -> Replay q r r
+
+ask :: q -> Replay q r r
 ask q = Ask q
-                
+
 
 
 emptyTrace :: Trace r
@@ -84,35 +92,3 @@ emptyTrace = []
 
 addAnswer  :: Trace r -> r -> Trace r
 addAnswer t r = t ++ [Answer r]
-
-
-example :: Replay String String Int
-example = do
-  --t0 <- io getCurrentTime
-  io (putStrLn "Hello!")
-  age <- ask "What is your age?"
-  io (putStrLn ("You are " ++ (show age)))
-  name <- ask "What is your name?"
-  io (putStrLn (name ++ " is " ++ age ++ " years old"))
-  -- t1 <- io getCurrentTime
-  -- io (putStrLn ("Total time: " ))
-  return (read age)
-
-  
-running :: Replay String String a -> IO a
-running prog = play emptyTrace
- where
-  play t = do
-    r <- run prog t    -- this is the same prog every time!
-    case r of
-      Left (q,t2) -> do
-        print t2
-        putStr ("Question: " ++ q ++ " ")
-        r <- getLine
-        play (addAnswer t2 r)
-      Right x -> do print t
-                    return x
-
- 
-
-                          
